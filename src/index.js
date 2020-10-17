5d4e2ccbc54c90a1c5434e1ec1cca896c2bc7fbb
Anna Startseva <a.v.starceva@gmail.com>
2020-10-18 02:34:09 +0600

    feat: add task resolution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === [] || matrix === undefined) { return [] };
+    let result = matrix.reduce((result, element, index) => result.concat(index % 2 !== 0 ? element.reverse() : element), []);
+    return result;
 }
